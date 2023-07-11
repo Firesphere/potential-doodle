254cffb7145f75eaf6dc0932f970eaa4b518718d
Simon Erkelens <simon@pikselin.com>
2023-07-12 11:56:21 +1200

    Nope, it's plus, not brackets

--- src/Extensions/PageControllerExtension.php
+++ src/Extensions/PageControllerExtension.php
@@ -117,6 +117,6 @@ class PageControllerExtension extends Extension
         }
 
         // Return a format like modulehelper@1.0.0+commithash
-        return sprintf('%s@%s (%s)', $appData['project'][1] ?? $appData['project'][0], trim($version), $appData['commit']);
+        return sprintf('%s@%s+%s', $appData['project'][1] ?? $appData['project'][0], trim($version), $appData['commit']);
     }
 }
